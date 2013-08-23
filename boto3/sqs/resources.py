d838b7ce6726e2e038e1ba5c5ce045a335dc591e
Daniel Lindsley <daniel@toastdriven.com>
2013-08-23 15:55:48 -0700

    Altered the SQS Resource classes to reflect the desired/possible API.
    Needs implementation, testing & sanity checking.

--- boto3/sqs/resources.py
+++ boto3/sqs/resources.py
@@ -1,42 +1,58 @@
-from boto3.core.resource import Resource
-from boto3.sqs.connection import SQSConnection
+from hashlib import md5
+
+from boto3.core.exceptions import MD5ValidationError
+from boto3.core.resource import Resource, Structure
+from boto3.core.resource import BoundField, ListBoundField
+from boto3.core.resource import InstanceMethod, ClassMethod
 
 
-class SQSQueue(Resource):
-    connection_class = SQSConnection
-    # Upon processing this, we'll also generate a reflected version to
-    # speed up processing output.
-    instance_vars = {
-        # instance_name: snake_case_service_name
-        'name': 'queue_name',
-        # ...
-    }
-    crud = {
-        'create': 'create_queue',
-        'delete': 'delete_queue',
-        # In this case, there is no 'read' or 'update' methods on the
-        # connection, so they stay unimplemented.
-    }
+class Queue(Resource):
+    # A special, required key identifying what API versions a given
+    # ``Resource/Structure`` works correctly with.
+    valid_api_versions = [
+        '2012-11-05',
+    ]
+
+    # Instance variables
+    name = BoundField('QueueName')
+    url = BoundField('QueueUrl', required=False)
+
+    # Methods
+    create = InstanceMethod('create_queue')
+    delete = InstanceMethod('delete_queue')
+    list_queues = ClassMethod('list_queues', kwargs={
+        'prefix': None
+    })
+    get_url = InstanceMethod('get_queue_url')
+    get_messages = InstanceMethod('receive_message', )
 
 
-class SQSMessage(Resource):
-    connection_class = SQSConnection
-    instance_vars = {
-        # Dotted syntax allows us to refer to instance data on another
-        # object...
-        'queue.url': 'queue_url',
-        'body': 'body',
-    }
-    # Added is a way to depend on a parent object.
-    depends_on = {
-        # instance_name: class_or_import_string
-        'queue': SQSQueue,
-        # Could have been ``'queue': 'boto3.sqs.resource.SQSQueue'`` to
-        # mitigate circular dependencies...
-    }
-    crud = {
-        'create': 'send_message',
-        'delete': 'delete_message',
-        'get': 'receive_message',
-        # No ``update`` is present in the API.
-    }
+class Attribute(Structure):
+    valid_api_versions = [
+        '2012-11-05',
+    ]
+
+    name = BoundField('Name')
+    value = BoundField('Value')
+
+
+class Message(Structure):
+    valid_api_versions = [
+        '2012-11-05',
+    ]
+
+    body = BoundField('Body')
+    md5 = BoundField('MD5OfBody', required=False)
+    message_id = BoundField('MessageId', required=False)
+    receipt_handle = BoundField('ReceiptHandle', required=False)
+    attributes = ListBoundField('Attribute', Attribute)
+
+    def post_populate(self):
+        # Verify the MD5 if present.
+        if not self.md5:
+            return
+
+        body_md5 = md5(self.body).hexdigest()
+
+        if body_md5 != self.md5:
+            raise MD5ValidationError("The provided MD5 does not match the body")
