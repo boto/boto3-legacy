c727b3ab7090ac621da74466f803e42958158b7c
Daniel Lindsley <daniel@toastdriven.com>
2013-10-15 17:06:30 -0700

    Use Botocore's method for now.

--- boto3/utils/mangle.py
+++ boto3/utils/mangle.py
@@ -1,13 +1,19 @@
+from botocore import xform_name
+
+
 def to_snake_case(camel_case_name):
-    bits = []
-
-    for char in camel_case_name:
-        if char.isupper():
-            bits.append('_')
-
-        bits.append(char.lower())
-
-    return ''.join(bits).lstrip('_')
+    # FIXME: Revisit this to make sure we're safe with the above import
+    #        and that we'd really prefer that behavior.
+    # bits = []
+    #
+    # for char in camel_case_name:
+    #     if char.isupper():
+    #         bits.append('_')
+    #
+    #     bits.append(char.lower())
+    #
+    # return ''.join(bits).lstrip('_')
+    return xform_name(camel_case_name)
 
 
 def to_camel_case(snake_case_name):
