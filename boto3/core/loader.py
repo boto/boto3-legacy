223f3ce41d15f6cd5c8ad0fb8794cc865f5cb3dd
Daniel Lindsley <daniel@toastdriven.com>
2013-11-15 16:00:37 -0800

    Added docstrings to the ``ResourceJSONLoader``.

--- boto3/core/loader.py
+++ boto3/core/loader.py
@@ -6,11 +6,44 @@ from boto3.utils import json
 
 
 class ResourceJSONLoader(object):
+    """
+    Handles the loading of the ResourceJSON. Can be overridden to look up
+    user-defined paths first, with fallbacks.
+
+    For optimal efficiency, one loader instance should be used in as many
+    places as possible & use the ``__getitem__`` (dictionary-style) lookups.
+    This will populate the cache, causing subsequent lookups to be fast & one
+    instance will prevent memory bloat.
+
+    Usage::
+
+        >>> rjl = ResourceJSONLoader()
+        # Load it no matter what.
+        >>> rjl.load('s3')
+        {
+            # ...S3's ResourceJSON as a dict...
+        }
+        # Get a cached version (or load it if not present).
+        >>> rjl['ec2']
+        {
+            # ...EC2's ResourceJSON as a dict...
+        }
+
+    """
     default_data_dirs = [
         DEFAULT_RESOURCE_JSON_DIR,
     ]
 
     def __init__(self, data_dirs=None):
+        """
+        Creates a new ``ResourceJSONLoader`` instance.
+
+        :param data_dirs: (Optional) A list of absolute paths to check for the
+            ResourceJSON. By default, this is just
+            ``boto3.core.constants.DEFAULT_RESOURCE_JSON_DIR`` as the single
+            item in the list.
+        :type data_dirs: list
+        """
         self.data_dirs = data_dirs
         self._loaded_data = {}
 
@@ -18,9 +51,31 @@ class ResourceJSONLoader(object):
             self.data_dirs = self.default_data_dirs
 
     def construct_filepath(self, path, service_name):
+        """
+        Constructs the expected path to load the JSON from.
+
+        :param path: The absolute path to a data directory.
+        :type path: string
+
+        :param service_name: The name of the desired service
+        :type service_name: string
+
+        :returns: The full path to a (potential) JSON file
+        """
         return os.path.join(path, "{0}.json".format(service_name))
 
     def load(self, service_name):
+        """
+        Loads the desired JSON for a service. (uncached)
+
+        This will fall back through all the ``data_dirs`` provided to the
+        constructor, returning the **first** one it finds.
+
+        :param service_name: The name of the desired service
+        :type service_name: string
+
+        :returns: The loaded JSON as a dict
+        """
         data = {}
 
         # Check the various paths for overridden versions. Take the first one
@@ -46,6 +101,18 @@ class ResourceJSONLoader(object):
         return data
 
     def __getitem__(self, service_name):
+        """
+        Loads the desired JSON for a service. (cached)
+
+        Identical to ``.load(...)``, this simply checks the cache first. If the
+        data is found, no I/O is performed. If not, a ``.load(...)`` is
+        performed, the result is cached & the data is then passed through.
+
+        :param service_name: The name of the desired service
+        :type service_name: string
+
+        :returns: The loaded JSON as a dict
+        """
         # Fetch from the cache first if it's there.
         if service_name in self._loaded_data:
             return self._loaded_data[service_name]
