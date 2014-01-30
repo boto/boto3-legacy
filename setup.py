2c5858179eb1834f7ca87590417ae2ef486dfce1
Daniel Lindsley <daniel@toastdriven.com>
2014-01-30 13:30:53 -0800

    Relicensed under Apache 2.0.

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'Natural Language :: English',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
