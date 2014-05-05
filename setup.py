3dee29e4d27567b017aebbed4825618eaab5670a
Romain Bossart <romain.bossart@gmail.com>
2014-05-05 23:55:33 +0200

    read version from __init__ instead of importing it

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ distutils/setuptools install script.
 """
 
 import os
+import re
 import sys
-import boto3
+import codecs
+# import boto3
 
 try:
     from setuptools import setup
@@ -14,6 +16,26 @@ try:
 except ImportError:
     from distutils.core import setup
 
+
+here=os.path.abspath(os.path.dirname(__file__))
+
+
+# Read the version number from a source file.
+# Why read it, and not import?
+# see https://groups.google.com/d/topic/pypa-dev/0PkjVpcxTzQ/discussion
+def find_version(*file_paths):
+    # Open in Latin-1 so that we avoid encoding errors.
+    # Use codecs.open for Python 2 compatibility
+    with codecs.open(os.path.join(here, *file_paths), 'r', 'latin1') as f:
+        version_file=f.read()
+
+    # The version line must have the form
+    # __version__='ver'
+    version_match=re.search(r"^__version__ = \(([^'\",]*),\s*([^'\",]*),\s*([^'\",]*).*\)",
+                            version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
 
 packages = [
     'boto3',
@@ -36,7 +58,8 @@ dependency_links = [
 
 setup(
     name='boto3',
-    version=boto3.get_version(),
+    # version=boto3.get_version(),
+    version=find_version('boto3', '__init__.py'),
     description='Low-level, data-driven core of boto 3.',
     long_description=open('README.rst').read(),
     author='Amazon Web Services',
